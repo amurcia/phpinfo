2f51f5c3123b88d49c41fea37c3e61b854f86a5c
Alvaro Murcia <amurcia@users.noreply.github.com>
2021-03-02 09:33:41 -0600

    Create script.php

---
+++ script.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?>
+  
+  
+  for ($x = 0; $x <= 10; $x++) {
+  echo "The number is: $x <br>";
+}
+
+</body>
+</html>
