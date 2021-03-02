2087e8bf3eec5e3b948aa72797fc30f52269a741
Alvaro Murcia <amurcia@users.noreply.github.com>
2021-03-02 10:00:02 -0600

    Update script.php

--- script.php
+++ script.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My first PHP page</h1>
-
 <?php
-echo "Hello World!";
-?>
-  
+
   
   for ($x = 0; $x <= 10; $x++) {
   echo "The number is: $x <br>";
 }
 
-</body>
-</html>
+?>
